6fa8f46d4e06e67d20051a16775e35064f81be25
Velihan <velihantpts@gmail.com>
2022-10-20 22:59:25 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './components/navbar/Header';
+
+function App() {
+  return (
+    <div>
+   <Header />
+   
+   <Header />
+    </div>
+
+  );
+}
+
+export default App;
